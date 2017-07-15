9f91c6037460612afcb32f7e3f31b9851a6cef04
fuzhenn <fuzhen@maptalks.org>
2017-07-15 18:58:30 +0800

    use include instead of proto

--- index.js
+++ index.js
@@ -1,32 +1,36 @@
 import isect from '2d-polygon-self-intersections';
 import * as maptalks from 'maptalks';
 
-maptalks.Polygon.prototype.isects = function () {
-    const coordinates = maptalks.Coordinate.toNumberArrays(this.getCoordinates());
-    const sects = [];
-    var r, ring;
-    for (let i = 0, l = coordinates.length; i < l; i++) {
-        ring = coordinates[i];
-        if (ring.length > 0) {
-            ring = ring.slice(0, ring.length - 1);
+maptalks.Polygon.include({
+    isects() {
+        const coordinates = maptalks.Coordinate.toNumberArrays(this.getCoordinates());
+        const sects = [];
+        let r, ring;
+        for (let i = 0, l = coordinates.length; i < l; i++) {
+            ring = coordinates[i];
+            if (ring.length > 0) {
+                ring = ring.slice(0, ring.length - 1);
+            }
+            r = isect(ring);
+            if (r.length > 0) {
+                sects.push([i, r]);
+            }
         }
-        r = isect(ring);
-        if (r.length > 0) {
-            sects.push([i, r]);
-        }
+        return sects;
     }
-    return sects;
-};
+});
 
-maptalks.MultiPolygon.prototype.isects = function () {
-    const geometries = this.getGeometries();
-    var r;
-    const sects = [];
-    for (let i = 0, l = geometries.length; i < l; i++) {
-        r = geometries[i].isects();
-        if (r.length > 0) {
-            sects.push([i, r]);
+maptalks.MultiPolygon.include({
+    isects() {
+        const geometries = this.getGeometries();
+        let r;
+        const sects = [];
+        for (let i = 0, l = geometries.length; i < l; i++) {
+            r = geometries[i].isects();
+            if (r.length > 0) {
+                sects.push([i, r]);
+            }
         }
+        return sects;
     }
-    return sects;
-};
+});
